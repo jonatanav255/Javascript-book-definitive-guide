8afaf335e8d596930122d8c03330aa2e47ba921a
Jonatan <jonatanav255@hotmail.com>
2025-01-26 20:31:49 -0600

    dates

--- src/chapter1/FirstExample.ts
+++ src/chapter1/FirstExample.ts
@@ -4,3 +4,14 @@ const si: boolean = true
 
 
 console.log("hi")
+
+let timestamp:number = Date.now();
+let now: Date = new Date();
+let ms: number = now.getTime();
+let iso: string = now.toISOString();
+
+
+console.log(timestamp)
+console.log(now)
+console.log(ms)
+console.log(iso)
