c21f6abf33878670c5967334958a24b397138b23
Jonatan <jonatanav255@hotmail.com>
2025-01-26 21:12:17 -0600

    playing around with /n

--- src/chapter1/FirstExample.ts
+++ src/chapter1/FirstExample.ts
@@ -15,3 +15,27 @@ console.log(timestamp)
 console.log(now)
 console.log(ms)
 console.log(iso)
+
+
+// A string representing 2 lines written on one line:
+const lineBreakString = 'two\nlines';
+console.log(lineBreakString);
+// Output:
+// two
+// lines
+
+// A one-line string written on 3 lines:
+const longString = "one\
+long\
+line";
+console.log(longString);
+// Output:
+// onelongline
+
+// A two-line string written on two lines:
+const multiLineString = `the newline character at the end of this line
+is included literally in this string`;
+console.log(multiLineString);
+// Output:
+// the newline character at the end of this line
+// is included literally in this string
